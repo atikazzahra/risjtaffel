c2e90758dff1957421542c7f1ef6af64dd1ebc40
atikazzahra <atikazzahra@live.com>
2018-04-17 16:19:54 +0700

    add routes for /restaurant

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 // Since the root/src dir contains our index.html
 app.use(express.static(__dirname + '/src/client/public'));
+app.use('/restaurant',express.static(__dirname + '/src/client/public'));
 
 // Heroku bydefault set an ENV variable called PORT=443
 //  so that you can access your site with https default port.
